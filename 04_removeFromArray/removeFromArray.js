5c0de0fd25171da0ac5d5464bf10300db51b48d0
Paubs13 <paulo.alessandro05@gmail.com>
2024-11-21 11:06:50 -0300

    first condition met

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, num) {
+    const newArray = [];
+    const length = array.length;
+    for (let i = 0; i < length; i++){
+        if (array[i] == num) {
+            continue;
+        }
+        else if (array[i] !== num) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray
 };
 
 // Do not edit below this line
