f1cfb96fd5a244b9c097040430e8e9349abad92f
Paubs13 <paulo.alessandro05@gmail.com>
2024-11-21 12:57:31 -0300

    Solution: Ez pz first if module determines conditions required in the tests. second if module determines whic variable is the lowest value. Then the for sums all values between the integers.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let higherValue;
+    let lowerValue;
+    if (a > 0 && b > 0 && (a % 1) == 0 && (b % 1) == 0 && typeof a === "number" && typeof b === "number"){
+        if (a > b) {
+            higherValue = a;
+            lowerValue = b;
+        }
+        else if (a < b) {
+            higherValue = b;
+            lowerValue = a;
+        }
+        let sum = lowerValue;
+        for(let counter = lowerValue + 1; counter <= higherValue; counter ++) {
+            sum = sum + counter;
+        }
+        return sum;
+    }
+    else {
+        return "ERROR"
+    } 
 };
 
 // Do not edit below this line
