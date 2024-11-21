6c20595a8527304bc5bb00a1d0eadedc0d75c2c4
Paubs13 <paulo.alessandro05@gmail.com>
2024-11-20 22:03:15 -0300

    01: just added hello hello World 02: i turned out to have troubles making the for statement and spent like 30 minutes until i figured it out. shame.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeat = string;
+    if (num > 0) {
+        for (let i = 1; i < num; i++){
+            repeat = repeat + string;
+        }
+        return repeat;
+    }
+    else if (num == 0) {
+        repeat = "";
+        return repeat;
+    }
+    else if (num < 0) {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
