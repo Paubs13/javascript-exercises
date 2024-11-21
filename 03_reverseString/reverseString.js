c7d91601e6122af18a6543787471b4522dbcf16d
Paubs13 <paulo.alessandro05@gmail.com>
2024-11-21 09:17:28 -0300

    Solution: discovered it exists array.reverse() but i supossed it wasn't meant to be resolved like that so i used a for cycle and then getted the index by multiplying it by -1.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const array = string.split('');
+    //const arrayReversed = array.reverse();  I guess it isn't desinged to be done like this.
+    length = array.length;
+    const arrayReversed = [];
+    let dirtyStringReversed = "";
+    for (let i = 1; i <= length; i++) {
+        let negativeIndex = i * -1;
+        let letter = array.at(negativeIndex);
+        dirtyStringReversed += letter;
+    }
+    return dirtyStringReversed;
 };
 
 // Do not edit below this line
