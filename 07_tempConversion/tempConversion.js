c59caf888c65775a3a69cb72c1f86c97a5bf3f1b
Paubs13 <paulo.alessandro05@gmail.com>
2024-11-21 17:06:40 -0300

    1 gooogle search away

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const celsTemp = (temp - 32) * (5/9);
+  let roundedTemp = Math.round(celsTemp * 10) / 10
+  return roundedTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const fahrTemp = ((temp * (9/5)) + 32);
+  let roundedTemp = Math.round(fahrTemp * 10) / 10
+  return roundedTemp;
 };
 
 // Do not edit below this line
